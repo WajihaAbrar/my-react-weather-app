a51661fd49e7464d191fd837589c84dded9ace12
WajihaAbrar <wajihaabrar@gmail.com>
2024-02-07 15:54:21 +0000

    Deleted weather.js and weather.css files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>React Weather App</h1>
-      {/* <Weather /> */}
     </div>
   );
 }
